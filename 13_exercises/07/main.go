d079cce089de157d5896e44f4dedf2afa297223a
SirWernich <melktert@gmail.com>
2017-01-21 20:24:57 +0200

    commented out redundant code

--- 13_exercises/07/main.go
+++ 13_exercises/07/main.go
@@ -6,14 +6,17 @@ func main() {
 	total := 0
 
 	for i := 1; i < 1000; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			total += i
-		} else if i%3 == 0 {
+		// not needed
+		//if i%3 == 0 && i%5 == 0 {
+		//	total += i
+		//} else if i%3 == 0 {
+
+		if i%3 == 0 {
 			total += i
 		} else if i%5 == 0 {
 			total += i
 		} else {
-			continue
+			continue // not *really* needed
 		}
 	}
 	fmt.Println("total of multiples of 3 and 5 is: ", total)
