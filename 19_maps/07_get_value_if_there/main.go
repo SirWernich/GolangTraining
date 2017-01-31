44b48db129a9a53d1f906280d4e38296fd8abf52
SirWernich <melktert@gmail.com>
2017-01-31 09:56:29 +0200

    incremental commit for maps lectures

--- 19_maps/07_get_value_if_there/main.go
+++ 19_maps/07_get_value_if_there/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	myMap := map[string]string{
+		"bob":   "abcde",
+		"sally": "qwerty",
+		"alex":  "banana",
+	}
+	fmt.Println(myMap)
+
+	// scope of "val" and "exists" is only in this if..else block
+	if val, exists := myMap["bob"]; exists {
+		fmt.Println("exists: ", exists)
+		fmt.Println("val: ", val)
+	} else {
+		fmt.Println("exists: ", exists)
+		fmt.Println("val: ", val)
+	}
+
+	// scope of "val" and "exists" is only in this if..else block
+
+	if val, exists := myMap["bobby"]; exists {
+		fmt.Println("exists: ", exists)
+		fmt.Println("val: ", val)
+	} else {
+		fmt.Println("exists: ", exists)
+		fmt.Println("val: ", val)
+	}
 }
