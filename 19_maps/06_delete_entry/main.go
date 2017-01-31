0569a1cf6923a835087a8f0afae48625d1893197
SirWernich <melktert@gmail.com>
2017-01-31 09:28:11 +0200

    incremental commit for maps lectures

--- 19_maps/06_delete_entry/main.go
+++ 19_maps/06_delete_entry/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	myMap := make(map[string]string)
+
+	myMap["bob"] = "seven"
+	myMap["maria"] = "twelve"
+	myMap["alex"] = "banana"
+	myMap["joe"] = "eight"
+
+	fmt.Println("before delete:", myMap)
+	delete(myMap, "alex")
+	delete(myMap, "alex") // doesn't throw error
+	fmt.Println("after delete:", myMap)
 }
