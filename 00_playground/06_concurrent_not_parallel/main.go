e0ebd37903b0448fa5516b8e24d5f52bc24a225e
SirWernich <melktert@gmail.com>
2017-02-02 06:20:00 +0200

    added one of todd's concurrent examples for later

---
+++ 00_playground/06_concurrent_not_parallel/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	fmt.Println("BEFORE")
+	fmt.Println("GO ROUTINES", runtime.NumGoroutine())
+	fmt.Println("number of logical CPUs usable by the current process", runtime.NumCPU())
+
+	for n := range c {
+		fmt.Println(n)
+	}
+
+	fmt.Println("AFTER")
+	fmt.Println("GO ROUTINES", runtime.NumGoroutine())
+	fmt.Println("number of logical CPUs usable by the current process", runtime.NumCPU())
+}
