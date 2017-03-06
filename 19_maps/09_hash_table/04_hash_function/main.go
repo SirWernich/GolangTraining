e7886e8bf84d32273a9b306fbef3bd3f872299c7
SirWernich <melktert@gmail.com>
2017-03-06 14:27:31 +0200

    unknown

--- 19_maps/09_hash_table/04_hash_function/main.go
+++ 19_maps/09_hash_table/04_hash_function/main.go
@@ -8,7 +8,10 @@ func main() {
 }
 
 func Hashbucket(word string, buckets int) int {
-	letter := rune(word[0])
+	// buckets is an int, rune is int32. to use rune, all int types
+	// will need to be changed to int32
+	//letter := rune(word[0])
+	letter := int(word[0])
 	bucket := letter % buckets
 	return bucket
 }
